a924569ffce740b0a96f5e070d44cc102d633da9
akaashvenkat <akaashvenkat@gmail.com>
2018-10-07 05:14:15 -0700

    update main

--- Source Code/cpp/main.cpp
+++ Source Code/cpp/main.cpp
@@ -16,7 +16,7 @@
 
 using namespace std;
 
-/*
+
 int main()
 {
     Socket socket;
@@ -32,9 +32,8 @@ int main()
             endID = socket.getEndID();
         }
         else
-        {
             continue;
-        }
+        
         Coordinate start, end;
         start = sqlconn.getStart(startID);
         end = sqlconn.getEnd(endID);
@@ -44,6 +43,7 @@ int main()
         
         vector< vector<string> > floorDetails;
         vector< vector< vector<int> > > cumulativePaths;
+        
         for (int i = 0; i < paths.size(); i++)
         {
             Path p = paths[i];
@@ -60,51 +60,9 @@ int main()
             floorDetails.push_back(floorDetail);
             cumulativePaths.push_back(floorNav);
         }
+        
         Instructor instructor(start, end, floorDetails, cumulativePaths);
         vector<string> guidance = instructor.getRouteGuidance();
         socket.write(guidance);
-    }
-}
- */
-
-int main()
-{
-    Socket socket;
-    SqlConn sqlconn;
-    
-    int startID = 23;
-    int endID = 44;
-    
-    Coordinate start, end;
-    start = sqlconn.getStart(startID);
-    end = sqlconn.getEnd(endID);
-        
-        PathManager pathManager(start, end);
-        vector<Path> paths = pathManager.getPaths();
-        
-        vector< vector<string> > floorDetails;
-        vector< vector< vector<int> > > cumulativePaths;
-        for (int i = 0; i < paths.size(); i++)
-        {
-            Path p = paths[i];
-            Point a = p.from;
-            Point b = p.to;
-            
-            Navigator nav(a.building, a.floor, a.xCoord, a.yCoord, b.xCoord, b.yCoord);
-            vector< vector<int> > floorNav = nav.navigate();
-            
-            vector<string> floorDetail;
-            floorDetail.push_back(to_string(a.building));
-            floorDetail.push_back(to_string(a.floor));
-            
-            floorDetails.push_back(floorDetail);
-            cumulativePaths.push_back(floorNav);
-        }
-        Instructor instructor(start, end, floorDetails, cumulativePaths);
-        vector<string> guidance = instructor.getRouteGuidance();
-    
-    for (int i = 0; i < guidance.size(); i++)
-    {
-        cout << guidance[i] << endl;
     }
 }
